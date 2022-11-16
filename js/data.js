5f1276f4c94093243a0fb96c5cb51c33d5a634a5
Baron Yahn <yahnbaron@me.com>
2022-11-16 21:56:31 +0000

    added store to local storage on unload

--- js/data.js
+++ js/data.js
@@ -5,3 +5,14 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataJSON = localStorage.getItem('mars-explorer');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+window.addEventListener('beforeunload', save);
+
+function save(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('mars-explorer', dataJSON);
+}
